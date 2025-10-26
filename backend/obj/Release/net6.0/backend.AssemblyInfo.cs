76f6d05853cf57c77db3948bd33d3effe059da18
Sanket Yerwadkar <sanketyerwadkar.github@gmail.com>
2025-10-26 09:06:32 +0530

    Initial commit: Angular + .NET project with Jekyll disabled

--- backend/obj/Release/net6.0/backend.AssemblyInfo.cs
+++ backend/obj/Release/net6.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03cfbe6e97dba1e8bec054023aae09fc024531cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
